a1aa78ed2c185f7bc1f226b9c9b1f7fb002020f7
karam-zomlut <zomlutk@gmail.com>
2022-06-01 07:22:49 +0300

    Finish Page Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { PageHeader } from './Components';
 class App extends Component {
   render() {
     return (
       <div className='container'>
-        {/* Components here */}
+        <main className='main-page'>
+          <PageHeader />
+        </main>
       </div>
     )
   }
