a0cef3ae46f6641a688b769c6cb26edb0499061a
Abd Elhadi <abdelhadips2020@gmail.com>
2022-06-01 21:49:55 +0300

    fix conditional delete task function

--- src/utils/deleteTask.js
+++ src/utils/deleteTask.js
@@ -1,5 +1,5 @@
 const deleteTask = (id, tasks) => {
-  const newTasksList = tasks.filter((item) => item.id === id);
+  const newTasksList = tasks.filter((item) => item.id !== id);
   return newTasksList;
 };
 
