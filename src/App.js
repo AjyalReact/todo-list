e093140fb1a94d1f2e05ef4badb3d8adec0c2ec5
karam-zomlut <zomlutk@gmail.com>
2022-06-01 07:11:53 +0300

    Setup The Page Structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>TO DO List</h1>
-    </div>
-  );
+import React, { Component } from 'react'
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        {/* Components here */}
+      </div>
+    )
+  }
 }
+
 
 export default App;
